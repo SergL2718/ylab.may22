9ee3c0f80c36bd1a42726507d90fddc713053a88
SergeyL <lcdorig@gmail.com>
2022-06-20 21:02:43 +0300

    less2 - Done !

---
+++ local/components/mylearn/promo/templates/.default/lang/ru/template.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['MYLEARN.PROMO.IF_YES'] = 'Можете ли вы участвовать в акции?';
+$MESS['MYLEARN.PROMO.YES'] = 'Да!';
+$MESS['MYLEARN.PROMO.NO'] = 'Нет:(';
+$MESS['MYLEARN.PROMO.ITEMS'] = 'Ваши товары в корзине:';
